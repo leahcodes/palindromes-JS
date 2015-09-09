5085e47370c3fb28039c79870e8f172de1747b9e
Leah Klamkin & Lisa Duschek <student@epicodus.com>
2015-09-09 14:29:41 -0700

    Converts the word into an array of single letters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 var palindrome = function(word) {
+  var wordArray = word.split("");
   if (word.length === 1) {
     return false;
-  };
+  } else {
+    return wordArray;
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,9 @@ describe('palindrome', function() {
    expect(palindrome("a")).to.equal(false);
   });
 });
+
+describe('palindrome', function() {
+  it("converts the word into an array of single letters", function() {
+   expect(palindrome("apple")).to.eql(["a", "p", "p", "l", "e"]);
+  });
+});
